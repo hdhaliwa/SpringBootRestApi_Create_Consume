b5f2ebdcfb29c9259b23e5fc2d6e770b856bb576
Hardeep Dhaliwal <hdhaliwal@thinkful.com>
2020-01-23 22:11:47 -0700

    first commit

---
+++ demoRC/src/main/java/com/client/CallRestService.java
@@ -0,0 +1,22 @@
+package com.client;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.model.Person;
+
+@Component
+public class CallRestService implements CommandLineRunner {
+	public static void callrestservice() {
+		RestTemplate restTemplate = new RestTemplate();
+		Person person = restTemplate.getForObject("http://localhost:8080/persons/1", Person.class);
+		System.out.println("Person first name is " +person.getFirstName());
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		callrestservice();
+		
+	}
+}
